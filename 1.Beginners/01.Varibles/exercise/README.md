e77bcf5db014c2591e0b70d7b946a7f7579c9ab7
CodingforMachine <codingformachine@gmail.com>
2025-04-11 23:29:47 +0500

    my commits

---
+++ 1.Beginners/01.Varibles/exercise/README.md
@@ -0,0 +1,7 @@
+1. O‘zgaruvchi nomi harf yoki pastki chiziq belgisi (_) bilan boshlanishi kerak.
+2. O‘zgaruvchi nomi raqam bilan boshlana olmaydi
+3. O‘zgaruvchi nomi faqat alfa-raqamli belgilar va pastki chiziqdan iborat bo‘lishi mumkin (a-z, A-Z, 0-9 va _ )
+4. Oʻzgaruvchilar nomlari katta-kichik harflarga sezgir (yosh, Age va AGE uch xil oʻzgaruvchidir)
+5. O‘zgaruvchi nomining uzunligiga cheklov yo‘q
+6. O‘zgaruvchi nomida bo‘shliq bo‘lishi mumkin emas
+7. O‘zgaruvchi nomi hech qanday python kalit so‘zlari bo‘lishi mumkin emas
---
+++ 1.Beginners/01.Varibles/exercise/main.py
@@ -0,0 +1,18 @@
+
+"""
+python--> 
+1. O‘zgaruvchi nomi harf yoki pastki chiziq belgisi (_) bilan boshlanishi kerak.
+2. O‘zgaruvchi nomi raqam bilan boshlana olmaydi
+3. O‘zgaruvchi nomi faqat alfa-raqamli belgilar va pastki chiziqdan iborat bo‘lishi mumkin (a-z, A-Z, 0-9 va _ )
+4. Oʻzgaruvchilar nomlari katta-kichik harflarga sezgir (yosh, Age va AGE uch xil oʻzgaruvchidir)
+5. O‘zgaruvchi nomining uzunligiga cheklov yo‘q
+6. O‘zgaruvchi nomida bo‘shliq bo‘lishi mumkin emas
+7. O‘zgaruvchi nomi hech qanday python kalit so‘zlari bo‘lishi mumkin emas
+"""
+
+name = "Asadbek"  # Pythonda (string) ma'lumot tuzilmasi, bu belgilar ketma-ketligini xotiraga saqlovchi tur (type) hisoblanadi.
+age = 23  # Python (int) ma'lumot tuzilmasi, bu matematikadagi butun sonlarga to'g'ri keladi. M: ... -4, -3, -2, -1, 0, 1, 2, 3, 4....
+is_active = True  # Boolean qiymatlar True yoki False qabul qiladi. Bu haqida keyinroq to'xtalamiz...
+
+# Ma'lumot tuzilmalari haqida hali qayg'urmang, bu haqida keyinroq to'xtalamiz.
+
